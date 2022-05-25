1652c14d9420e7e4c6a1fdaa09ff3e237de5734f
AlikhanovN <alikhanov@yahoo.com>
2022-05-25 09:59:43 +0400

    add photo format in checker.py

--- school_tasks/in_progress/checker.py
+++ school_tasks/in_progress/checker.py
@@ -1,12 +1,17 @@
 print("Checker")
 age = int(input("Enter age: "))
+photo = input("Add photo: ")
 language = input("What language do you speak: ")
 examples = "english", "spasnish", "russian", "kyrgyz"
+test = ".jpg", ".png"
 if 0 > age:
     print("Incorrect age")
 elif age > 150:
     print("Incorrect age")
 elif language not in examples:
     print("Incorrect language")
+elif photo[photo.find(".")::] not in test:
+    print("Incorrect photo format")
 else:
     print("Checked in")
+
