b5cf93d688c1042492cd54b8037c8eb61c355792
AlikhanovN <alikhanov@yahoo.com>
2022-05-26 07:33:09 +0400

    add tasks in to file

--- school_tasks/in_progress/new_team_project.py
+++ school_tasks/in_progress/new_team_project.py
@@ -1,1 +1,15 @@
-Hi :)
+'''
+Task 1 - Aidana
+Task 2 - Aidana
+Task 5 - Aidana
+'''
+
+print("Task 3")
+a = 4729461084
+counter = 0
+while a:
+    counter += a % 10
+    a //= 10
+print(counter)
+
+print("Task 4")
