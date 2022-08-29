80b39b2136639997d84739746fac2e7c5088a614
AlikhanovN <alikhanov@yahoo.com>
2022-08-29 20:51:30 +0400

    Add flask project

---
+++ school_tasks/my-first-blog.git/app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import News
+
+# Create your views here.
+def index(request):
+    news = News.objects.all()
+    return render(request, "index.html", {"data" : news})
